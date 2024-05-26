5bb37002b3314f7487163b9c1001e08c13b6facf
NgQuangHuyit <nguyenquanghuy240803@gmail.com>
2024-05-26 19:36:11 +0700

    update metabase dashboard

--- dags/ddl/create_dim_customer.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-def create_tables(
-        spark: SparkSession,
-        path: str = 'hdfs://hadoop-namenode:9000/datalake/raw/',
-        schema: str = 'oltp',
-):
-    return None
